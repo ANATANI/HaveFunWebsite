56335f79fc527a58bc2c63b5155e1df626b7d446
DESKTOP-7R9U3TR\HUIYUN <winny14752@gmail.com>
2024-07-25 18:54:20 +0800

    retain parts by HUIYUN

---
+++ HaveFun/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+using HaveFun.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Net.Mail;
+
+namespace HaveFun.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
